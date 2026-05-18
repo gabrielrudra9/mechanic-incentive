1ecb2af3cead6c513440a2d4be97327ac37cbe57
gabrielrudra9 <gabrielrudra9@gmail.com>
2026-05-18 15:45:38 +0700

    Fix: use SvelteKit proper config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { sveltekit } from '@sveltejs/kit/vite'
 
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [sveltekit()],
   preview: {
     host: '0.0.0.0',
     port: 4173,
